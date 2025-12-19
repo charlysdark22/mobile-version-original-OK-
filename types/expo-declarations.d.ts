8bdf6ee98d997627cde9be761e7570965c67ff52
charly <carlosaguilardark22@gmail.com>
2025-12-18 22:18:15 -0500

    chore: update Gradle wrapper to version 8.14.3 and add necessary scripts
    feat: configure settings.gradle for React Native and Expo integration
    
    chore: add TypeScript declarations for Expo modules

---
+++ types/expo-declarations.d.ts
@@ -0,0 +1,19 @@
+declare module '@react-native-async-storage/async-storage' {
+  const AsyncStorage: any;
+  export default AsyncStorage;
+}
+
+declare module 'expo-file-system' {
+  const fs: any;
+  export = fs;
+}
+
+declare module 'expo-sharing' {
+  const sharing: any;
+  export = sharing;
+}
+
+declare module 'expo-document-picker' {
+  const picker: any;
+  export = picker;
+}
